e9effb8b61441bc894b80c377a517919052a5ae0
PawelPotter1 <pawel.figulus@gmail.com>
2021-02-07 10:33:24 +0100

    render

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const { response } = require("express");
 const express = require("express");
+const path = require("path");
+const { Script } = require("vm");
+
 const port = 3000;
 const app = express();
 
+app.set("view engine",'hbs')
+app.use('/assets',express.static(path.join(__dirname,"./assets")))
 app.get("/", function (req,res) {
-    res.send("<h4>Strona o nas</h4>")
+    res.render(`index`)
+})
+
+app.use('/js',express.static(path.join(__dirname, "./js")));
+
+app.use('/js',express.static(__dirname + './../public/js'));
+
+app.get('/about', function (req, res){
+    response.send("My site")
 })
 
 app.listen(port, (err) => {
