4d1289e717f080dad19bf3b3a13710fafc6a5a89
PawelPotter1 <pawel.figulus@gmail.com>
2021-02-07 09:27:57 +0100

    prosty komunikat

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require("http");
-const server = http.createServer();
+
+const handler = (request, response) => {
+    response.end("<p> Hello OK </p>")
+    console.log("działa")
+    }
+
+const server = http.createServer(handler);
 const port = 3000;
 
-server.listen(port, () => {
-    console.log('serwer działa na porcie ${port}')
+server.listen(port, (err) => {
+    if (err) {return console.log("błąd",err)}
 })
